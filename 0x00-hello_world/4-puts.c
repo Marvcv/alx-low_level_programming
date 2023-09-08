aa9f5ddc27e7c4f24c382e8247e8a7f2bfc3cabe
marvcv <librarianmarvin@gmail.com>
2023-09-08 04:13:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -it prints out output using put
+ *
+ * return :always (o)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

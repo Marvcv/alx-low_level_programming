be83e8765eae940de1616e28af61d3e15e1267a8
marvcv <librarianmarvin@gmail.com>
2023-09-13 04:23:38 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * main - Entry point
+ *
+ * Description: This is the entry point of the program.
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; 1 <= 7, 1++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+
+	return (0) ;
+}

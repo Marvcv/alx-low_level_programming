8defa593c04b3bb3f1d220706344cf96e6829bdf
marvcv <librarianmarvin@gmail.com>
2023-09-18 14:30:37 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,18 +7,9 @@
  */
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
 	int o;
 
-	while (*y != '\0')
-	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
+	for (o = 0 ; str[o] != '\0' ; o++)
 	{
 		if (o % 2 == 0)
 	{

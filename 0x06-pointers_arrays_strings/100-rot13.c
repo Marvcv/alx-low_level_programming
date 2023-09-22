208399b795ad4390b0ec0e04bc82252d73ae8516
marvcv <librarianmarvin@gmail.com>
2023-09-22 12:34:47 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
- *  * rot13 - encoder rot13
- *   * @s: pointer to string params
- *    *
- *     * Return: *s
+ *  rot13 - encoder rot13
+ *  @s: pointer to string params
+ *  Return: *s
  */
 
 char *rot13(char *s)

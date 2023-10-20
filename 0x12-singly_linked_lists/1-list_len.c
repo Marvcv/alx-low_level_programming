1bfc2e249643848adb99bd29208a600487a2799c
marvcv <librarianmarvin@gmail.com>
2023-10-20 10:25:22 -0700

    ds

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,13 @@
 #include "dog.h"
 
 /**
- * init_dog - Initializes a variable of type struct dog.
- * @d: A pointer to a struct dog to be initialized.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * init_dog - initializes a variable of type struct dog.
+ * @d: struct dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
  *
- * Description: This function takes a pointer to a struct dog and initializes
- * its members with the provided values for the name, age, and owner. It ensures
- * that the pointer to the struct is not NULL before performing the initialization.
- *
- * Return: No return value.
+ * Return: no return.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -22,4 +18,3 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->owner = owner;
 	}
 }
-

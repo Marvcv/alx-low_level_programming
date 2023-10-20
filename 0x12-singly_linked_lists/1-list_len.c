154014e413b65477ad6d8878a3e2778620d82981
marvcv <librarianmarvin@gmail.com>
2023-10-20 10:33:36 -0700

    sed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a linked list.
- * @h: Head of the linked list.
+ * list_len - Returns the number of elements in a linked list.
+ * @h: Pointer to the head of the linked list.
  *
  * Description: This function counts the number of nodes in a linked list.
  *
  * Return: The number of nodes in the linked list.
  */
-size_t listint_len(const listint_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t nnodes = 0;
+	unsigned int i = 0;
 
 	while (h != NULL)
 	{
+		i++;
 		h = h->next;
-		nnodes++;
 	}
-
-	return (nnodes);
+	return (i);
 }
